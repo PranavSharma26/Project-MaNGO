ea1b3a61d9ce6df82fb8b4e79d8e55745978daba
PranavSharma26 <pranavsharma12355@gmail.com>
2024-09-03 16:47:17 +0530

    frontend URL changed

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -22,7 +22,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post("https://mango-backend1.vercel.app/user/signup", userInfo)
       .then((res) => {
         if (res.data) {
           toast.success("Signup Successfully");
